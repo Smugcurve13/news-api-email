5637dc42d4bb51c97d3974256b60cae35922b9f3
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-08 17:05:25 +0530

    sends email about news of a topic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = 'b02c08b3f9a44c378767ddefd5d87ddc'
 url = f'https://newsapi.org/v2/everything?q=anime&from=2024-09-09&sortBy=publishedAt&apiKey={api_key}'
@@ -9,7 +10,13 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
+body = ''
+
 # Access the article titles and descriptions
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    body = body + article['title']+'\n'+article['description']+'\n\n'
+    
+# message = ''.join(body)
+
+message = body.encode('utf-8')
+send_email(message=message)
