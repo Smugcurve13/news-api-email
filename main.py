86671818f8d4a949caced6af9abb3de0e479b193
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-08 15:42:30 +0530

    access news titles and descriptions

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import requests
 
 api_key = 'b02c08b3f9a44c378767ddefd5d87ddc'
-url = f'https://newsapi.org/v2/everything?q=nvidia&from=2024-09-09&sortBy=publishedAt&apiKey={api_key}'
+url = f'https://newsapi.org/v2/everything?q=anime&from=2024-09-09&sortBy=publishedAt&apiKey={api_key}'
 
+# Make Request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and descriptions
+for article in content['articles']:
+    print(article['title'])
+    print(article['description'])
