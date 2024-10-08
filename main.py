6216d5bf8fd22f229a634450c1410e8fece8a2ad
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-08 15:05:37 +0530

    added url , api key and requests

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import requests
+
+api_key = 'b02c08b3f9a44c378767ddefd5d87ddc'
+url = f'https://newsapi.org/v2/everything?q=nvidia&from=2024-09-09&sortBy=publishedAt&apiKey={api_key}'
+
+request = requests.get(url)
+content = request.text
+print(content)
