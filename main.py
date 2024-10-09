5b9e7e26fa7a96d78b6fb987c226ceaaaf9c6775
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-09 10:00:50 +0530

    subject show correctly now

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import requests
 from send_email import send_email
 
+topic = 'india'
+
 api_key = 'b02c08b3f9a44c378767ddefd5d87ddc'
-url = f'https://newsapi.org/v2/everything?q=anime&from=2024-09-09&sortBy=publishedAt&apiKey={api_key}'
+url = 'https://newsapi.org/v2/everything?'\
+f'q={topic}&'\
+'from=2024-09-09&'\
+'sortBy=publishedAt&'\
+f'apiKey={api_key}&'\
+'language=en'
 
 # Make Request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-
+subject = "subject: Today's Samachar \n"
 body = ''
 
 # Access the article titles and descriptions
-for article in content['articles']:
-    body = body + article['title']+'\n'+article['description']+'\n\n'
+for article in content['articles'][:20]:
+    body += subject
+    if article['title'] is not None:
+        body = body + article['title']+'\n'\
+        + article['description']+'\n' +\
+        article['url']+'\n\n'\
     
-# message = ''.join(body)
+# message = subject + body
 
 message = body.encode('utf-8')
-send_email(message=message)
+send_email(message=message) 
